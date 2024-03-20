bbdf4357c432d849b2455c132edadc23b8b185b9
AlvarowJS <alvaruwiiu@hotmail.com>
2024-03-20 00:20:54 -0500

    aputnando a produccion

--- src/api/bdCitas.js
+++ src/api/bdCitas.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const bdCitas = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api'
-    // baseURL: 'https://backend.tms2.nuvola7.com.mx/api'
+    // baseURL: 'http://127.0.0.1:8000/api'
+    baseURL: 'https://backend.tms2.nuvola7.com.mx/api'
 })
 
 export default bdCitas
