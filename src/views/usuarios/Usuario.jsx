f8d0c3d7c3cdb2d066ae6b543b4eeb3cd2620fa4
AlvarowJS <alvaruwiiu@hotmail.com>
2024-04-24 19:51:52 -0500

    validacion de usuario

--- src/views/usuarios/Usuario.jsx
+++ src/views/usuarios/Usuario.jsx
@@ -1,55 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Col, Input, Label, Row } from 'reactstrap'
-import { useForm } from 'react-hook-form'
-import bdCitas from '../../api/bdCitas'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
-import TablaUsuario from './TablaUsuario'
-import FormUsuario from './FormUsuario'
-const MySwal = withReactContent(Swal)
-const URL = '/v1/users'
+import React, { useEffect, useState } from "react";
+import { Button, Col, Input, Label, Row } from "reactstrap";
+import { useForm } from "react-hook-form";
+import bdCitas from "../../api/bdCitas";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import TablaUsuario from "./TablaUsuario";
+import FormUsuario from "./FormUsuario";
+const MySwal = withReactContent(Swal);
+const URL = "/v1/users";
 
 const Usuario = () => {
-  const token = localStorage.getItem('token')
-  const [data, setData] = useState()
-  const [search, setSearch] = useState()
-  const [filter, setFilter] = useState()
-  const [modal, setModal] = useState(false)
-  const [modalUbicacion, setModalUbicacion] = useState(false)
-  const [actualizacion, setActualizacion] = useState(false)
-  const { handleSubmit, register, reset } = useForm()
-  const [refresh, setRefresh] = useState(false)
+  const token = localStorage.getItem("token");
+  const [data, setData] = useState();
+  const [search, setSearch] = useState();
+  const [filter, setFilter] = useState();
+  const [modal, setModal] = useState(false);
+  const [modalUbicacion, setModalUbicacion] = useState(false);
+  const [actualizacion, setActualizacion] = useState(false);
+  const { handleSubmit, register, reset } = useForm();
+  const [refresh, setRefresh] = useState(false);
   const defaulValuesForm = {
-    nombres: '',
-    apellidos: '',
-    email: '',
-    password: '',
-    status: '',
-  }
+    nombres: "",
+    apellidos: "",
+    email: "",
+    password: "",
+    status: "",
+  };
   const getAuthHeaders = () => ({
     headers: {
-      'Authorization': 'Bearer ' + token
-    }
+      Authorization: "Bearer " + token,
+    },
   });
   const toggle = () => {
-    setActualizacion(false)
-    reset(defaulValuesForm)
-    setModal(!modal)
-  }
+    setActualizacion(false);
+    reset(defaulValuesForm);
+    setModal(!modal);
+  };
 
   const toggleActualizacion = () => {
-    setModal(!modal)
-  }
+    setModal(!modal);
+  };
 
   useEffect(() => {
-    bdCitas.get(`${URL}`, getAuthHeaders())
-      .then(res => {
-        setData(res.data)
+    bdCitas
+      .get(`${URL}`, getAuthHeaders())
+      .then((res) => {
+        setData(res.data);
       })
-      .catch(err => {
-
-      })
-  }, [refresh])
+      .catch((err) => {});
+  }, [refresh]);
   const handleFilter = (e) => {
     setSearch(e.target.value);
   };
@@ -58,134 +57,143 @@ const Usuario = () => {
 
   // Crear Usuario
   const crearUsuario = (data) => {
-    bdCitas.post(URL, data, getAuthHeaders())
-      .then(res => {
-        reset(defaulValuesForm)
-        toggle.call()
-        setRefresh(!refresh)
+    bdCitas
+      .post(URL, data, getAuthHeaders())
+      .then((res) => {
+        reset(defaulValuesForm);
+        toggle.call();
+        setRefresh(!refresh);
         Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Usuario creado',
+          position: "center",
+          icon: "success",
+          title: "Usuario creado",
           showConfirmButton: false,
-          timer: 1500
-        })
+          timer: 1500,
+        });
       })
-      .catch(err => {
-        Swal.fire({
-          position: 'center',
-          icon: 'error',
-          title: 'Contacte con soporte',
-          showConfirmButton: false,
-        })
-      })
-  }
+      .catch((err) => {
+        if (err.response.status == 422) {
+          Swal.fire({
+            position: "center",
+            icon: "error",
+            title: "Email registrado, por favor ingrese uno diferente",
+            showConfirmButton: false,
+          });
+        } else {
+          Swal.fire({
+            position: "center",
+            icon: "error",
+            title: "Contacte con soporte",
+            showConfirmButton: false,
+          });
+        }
+      });
+  };
 
   // Actualiza Consultorio (PUT)
   const actualizarUsuario = (id, data) => {
-    bdCitas.put(`${URL}/${id}`, data, getAuthHeaders())
-      .then(res => {
-        reset(defaulValuesForm)
-        toggle.call()
-        setRefresh(!refresh)
+    bdCitas
+      .put(`${URL}/${id}`, data, getAuthHeaders())
+      .then((res) => {
+        reset(defaulValuesForm);
+        toggle.call();
+        setRefresh(!refresh);
         Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Usuario Actualizado',
+          position: "center",
+          icon: "success",
+          title: "Usuario Actualizado",
           showConfirmButton: false,
-          timer: 1500
-        })
+          timer: 1500,
+        });
       })
-      .catch(err => {
+      .catch((err) => {
         Swal.fire({
-          position: 'center',
-          icon: 'error',
-          title: 'Contacte con soporte',
+          position: "center",
+          icon: "error",
+          title: "Contacte con soporte",
           showConfirmButton: false,
-        })
-      })
-  }
+        });
+      });
+  };
   const eliminarUsuario = (id) => {
     return MySwal.fire({
-      title: '¿Estás seguro de eliminar?',
+      title: "¿Estás seguro de eliminar?",
       text: "¡No podrás revertir esto!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonText: 'Si',
+      confirmButtonText: "Si",
       customClass: {
-        confirmButton: 'btn btn-primary',
-        cancelButton: 'btn btn-outline-danger ms-1'
+        confirmButton: "btn btn-primary",
+        cancelButton: "btn btn-outline-danger ms-1",
       },
-      buttonsStyling: false
+      buttonsStyling: false,
     }).then(function (result) {
       if (result.value) {
-        bdCitas.delete(`${URL}/${id}`, getAuthHeaders())
-          .then(res => {
-            setRefresh(!refresh)
+        bdCitas
+          .delete(`${URL}/${id}`, getAuthHeaders())
+          .then((res) => {
+            setRefresh(!refresh);
             Swal.fire({
-              position: 'center',
-              icon: 'success',
-              title: 'Consultorio Eliminado',
+              position: "center",
+              icon: "success",
+              title: "Consultorio Eliminado",
               showConfirmButton: false,
-              timer: 1500
-            })
+              timer: 1500,
+            });
           })
-          .catch(err => {
+          .catch((err) => {
             Swal.fire({
-              position: 'center',
-              icon: 'error',
-              title: 'Contacte con soporte',
+              position: "center",
+              icon: "error",
+              title: "Contacte con soporte",
               showConfirmButton: false,
-            })
-          })
+            });
+          });
       }
-    })
-
-
-  }
+    });
+  };
 
   // Tomara los datos que tiene un registro
   const actualizarUsuarioId = (id) => {
-    toggleActualizacion.call()
-    setActualizacion(true)
-    bdCitas.get(`${URL}/${id}`, getAuthHeaders())
-      .then(res => {
-        reset(res.data)
+    toggleActualizacion.call();
+    setActualizacion(true);
+    bdCitas
+      .get(`${URL}/${id}`, getAuthHeaders())
+      .then((res) => {
+        reset(res.data);
       })
-      .catch(err => null)
-  }
+      .catch((err) => null);
+  };
 
   // Si es actualizacion llamara a actualizarPaciente pero si es false crear un Consultorio
   const submit = (data) => {
     if (actualizacion) {
-      actualizarUsuario(data.id, data)
+      actualizarUsuario(data.id, data);
     } else {
-      crearUsuario(data)
+      crearUsuario(data);
     }
-  }
+  };
 
   return (
     <>
       <Row>
-        <Col sm='6'>
-          <Label className='me-1' for='search-input'>
+        <Col sm="6">
+          <Label className="me-1" for="search-input">
             Buscar
           </Label>
           <Input
-            className='dataTable-filter'
-            type='text'
-            bsSize='sm'
-            id='search-input'
-            placeholder='buscar por nombre y apellidos'
+            className="dataTable-filter"
+            type="text"
+            bsSize="sm"
+            id="search-input"
+            placeholder="buscar por nombre y apellidos"
             onChange={handleFilter}
           />
         </Col>
-        <Col sm='4'>
+        <Col sm="4"></Col>
 
-        </Col>
-  
-        <Col sm='2' className='mt-2'>
-          <Button onClick={toggle} color='primary'>
+        <Col sm="2" className="mt-2">
+          <Button onClick={toggle} color="primary">
             + Agregar
           </Button>
         </Col>
@@ -206,9 +214,8 @@ const Usuario = () => {
         reset={reset}
         getAuthHeaders={getAuthHeaders}
       />
-      
     </>
-  )
-}
+  );
+};
 
-export default Usuario
+export default Usuario;
